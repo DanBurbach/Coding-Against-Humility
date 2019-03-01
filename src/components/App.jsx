86d9e324768ea250c171c71e78094eb72fe8f217
Student <example@example.com>
2019-03-01 15:30:35 -0800

    add framework of loading screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,8 @@ import { Switch, Route } from 'react-router-dom';
 // core navigation and return information
 import Error404 from './Error404';
 
+// loading screen
+import LoadingScreen from '../components/LoadingScreen/Loading';
 
 // main splash page aka Home
 import Main from './Main';
@@ -14,20 +16,32 @@ import Fineprint from './Menu/Fineprint';
 
 // outlying component pages
 
-function App () {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  onShowLoading() {
+    loaderStatus.next(true);
+    setTimeout(() => {
+      loaderStatus.next(false);
+    }, 3000);
+  }
 
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route to path="/newgame" component={NewGame} />
-        <Route to path="/info" component={Info} />
-        <Route to path="/fineprint" component={Fineprint} />
 
-        <Route to component={Error404} />
-      </Switch>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <LoadingScreen />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route to path="/newgame" component={NewGame} />
+          <Route to path="/info" component={Info} />
+          <Route to path="/fineprint" component={Fineprint} />
+          <Route to component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
