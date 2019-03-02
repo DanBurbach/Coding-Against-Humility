dc38eb36f1c5eee984850aec6ad67b07e3c54d88
Student <example@example.com>
2019-03-01 16:50:02 -0800

    add reactive header with ability for touch screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../assets/styles/Header.css';
+import '../assets/styles/Header.js';
 
 function Header(){
-  const menubutton = {
-    color: 'white',
-    textDecoration: 'none'
-  };
-
-  const yourscore = {
-    color: 'white',
-    textDecoration: 'none',
-    textAlign: 'right'
-  };
-
   return (
     <div>
-      <button><Link to='/' style={menubutton}>Menu</Link></button>
-      <p style={yourscore}>Your score: '0'</p>
-      <p>Your Name:</p>
+      <nav id="nav" role="navigation">
+        <a href="#nav" title="Show navigation">Show navigation</a>
+        <a href="#" title="Hide navigation">Hide navigation</a>
+        <ul>
+          <li><Link to='/'>Menu</Link></li>
+          <li><Link to='/Info'>Gameplay</Link></li>
+          <li>Your score: '0'</li>
+          <li>Your Name:''</li>
+        </ul>
+      </nav>
     </div>
   );
 }
