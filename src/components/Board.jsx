bc7f402a755c52510288f0b9b4057afe45781761
Student <example@example.com>
2019-03-01 16:51:27 -0800

    add Header imported routing

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import MainPlayer from './MainPlayer/MainPlayer';
+import Header from './Header';
 
 function Board(){
-  return ()
+  return (
+    <div>
+      <Header />
+      <MainPlayer />
+    </div>
+  );
 }
 
 export default Board;
