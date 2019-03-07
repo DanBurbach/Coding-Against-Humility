ecbea28d516837486815bfd3e0784ee6a803c205
Student <example@example.com>
2019-03-07 14:44:53 -0800

    add index actions file and add action controllers

--- src/actions/WhtDeck.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function WhtDeck(){
-  return ()
-}
-
-export default WhtDeck;
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,48 @@
 import * as types from './../constants/ActionTypes';
+import v4 from 'uuid/v4';
 
-export const nextLyric = (currentSongId) => ({
-  type: types.NEXT_LYRIC,
-  currentSongId
+export const addWhiteCard = () => ({
+  type: types.ADD_WHITE_CARD,
+  newWhiteCard
 });
+
+export const removeWhiteCard = (currentWhiteCardID) => ({
+  type: types.REMOVE_WHITE_CARD,
+  currentWhiteCardID
+});
+
+export const addBlackCard = (currentBlackCardID) => ({
+  type: types.ADD_BLACK_CARD,
+  currentBlackCardID
+});
+
+export const removeBlackCard = (currentBlackCardID) => ({
+  type: types.REMOVE_BLACK_CARD,
+  currentBlackCardID
+});
+
+export function drawNewBlackCard(blackcards) {
+  return function (dispatch) {
+    const localBlackCardID = v4();
+    dispatch(addBlackCard(blackcards, localBlackCardID));
+    return fetch().then(
+      response => response.json(),
+      error => console.log('An error did occur when pulling a black card', error)
+    ).then(function(json) {
+      dispatch(addBlackCard)
+    })
+  };
+}
+
+export function drawNewWhiteCard(whitecards) {
+  return function (dispatch) {
+    const localWhiteCardID = v4();
+    dispatch(addWhiteCard(whitecards, localWhiteCardID));
+    return fetch().then(
+      response => response.json(),
+      error => console.log('An error did occur when pulling a white card', error)
+    ).then(function(json) {
+      dispatch(addWhiteCard)
+    })
+  };
+}
