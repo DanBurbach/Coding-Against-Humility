8870ecdce2c219cb807501d3a12c58d246c87945
Student <example@example.com>
2019-03-08 15:12:10 -0800

    compile files used in NewGame onto one Class state page of NewGame. Work on passing name, number of players, length of game in console log

--- src/components/Menu/NewGame.jsx
+++ src/components/Menu/NewGame.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import RadioGameLength from './RadioGameLength';
 import PropTypes from 'prop-types';
 import { v4 } from 'uuid';
 
@@ -7,54 +6,117 @@ class NewGame extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      _name: 'null',
-      _players: 'null',
-      _gamelength: 'null'
+      _name: null,
+      _players: null,
+      _gamelength: null,
     };
     this.handleNewGameSubmission = this.handleNewGameSubmission.bind(this);
+    this.handleNewPlayerName = this.handleNewPlayerName.bind(this);
+    this.handlePlayerNumbers = this.handlePlayerNumbers.bind(this);
+    this.handleLengthChoice = this.handleLengthChoice.bind(this);
   }
 
   handleNewGameSubmission(props) {
     event.preventDefault();
-    props.onNewGameSubmission({name: _name.value, players: _players.value, _gamelength: this.state.selectedGameLength, id: v4()
+    props.onNewGameSubmission({_name: _name.value, _players: _players.value, _gamelength: this.state.selectedGameLength, id: v4()
     });
-
     _name.value = '';
     _players.value = '';
     _gamelength.value = '';
   }
 
+  handleNewPlayerName(event) {
+    this.setState({nameValue: event._name.value});
+    console.log(event._name.value);
+  }
+
+  handleLengthChoice(changeEvent) {
+    this.setState({selectedGameLength: changeEvent.target.value});
+    console.log('Your game length is:', this.state.selectedGameLength);
+  }
+
+  handlePlayerNumbers(event) {
+    this.setState({playersValue: event._players.value});
+    console.log(event._players.value);
+  }
+
   render() {
     return (
       <div>
-        <form onSubmit={handleNewGameSubmission}>
+        <form onSubmit={this.handleNewGameSubmission}>
           <p>Your Name:</p>
-          <input
-            type='text'
-            id='name'
-            placeholder='Name'
-            ref={(input) => {_name = input;}}/>
+          <div>
+            <input type="text" value={this.state._name} onChange={this.handleNewPlayerName} />
+          </div>
+          <br/>
 
-          <p>Number of players (between 3 and 5):</p>
+          <p>Number of players (between 3 and 8):</p>
           <input
-            type='number'
-            name='quantity'
+            type='range'
+            name='_players'
             min='3'
             max='8'
-            value='3'/>
+            value={this.state._players}
+            onChange={this.handlePlayerNumbers}/>
+          <br/>
 
           <p>Game length:</p>
-          <RadioGameLength />
+          <div className='container'>
+            <div className='row'>
+              <div className='form-check'>
+                <label>
+                  <input
+                    type='radio'
+                    name='_gamelength'
+                    defaultValue='5'
+                    checked={this.state.selectedGameLength === '5'}
+                    onChange={this.handleLengthChoice}
+                    className='form-check-input'
+                  />
+                  Short
+                </label>
+              </div>
+              <br/>
+              <div className='form-check'>
+                <label>
+                  <input
+                    type='radio'
+                    name='_gamelength'
+                    defaultValue='7'
+                    checked={this.state.selectedGameLength === '7'}
+                    onChange={this.handleLengthChoice}
+                    className='form-check-input'
+                  />
+                  Medium
+                </label>
+              </div>
+              <br/>
+              <div className='form-check'>
+                <label>
+                  <input
+                    type='radio'
+                    name='_gamelength'
+                    defaultValue='10'
+                    checked={this.state.selectedGameLength === '10'}
+                    onChange={this.handleLengthChoice}
+                    className='form-check-input'
+                  />
+                  Long
+                </label>
+              </div>
+            </div>
+          </div>
+          <br/>
 
-          <p><input type="submit" value="Submit"></input></p>
-          <p><input type="reset"></input></p>
+          <p><input type="submit" value="Submit"></input> | <input type="reset"></input></p>
         </form>
       </div>
     );
   }
 }
 NewGame.propTypes = {
-  onNewGameSubmission: PropTypes.func
+  onNewGameSubmission: PropTypes.func,
+  handleNewGameSubmission: PropTypes.func
 };
 
 export default NewGame;
