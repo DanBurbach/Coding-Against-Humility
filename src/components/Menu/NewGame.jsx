042d6eb042438f78b65e09e74b5c1bd86af2c7a7
Student <example@example.com>
2019-03-08 12:24:39 -0800

    work on passing name of player and number of players onto main Game for use

--- src/components/Menu/NewGame.jsx
+++ src/components/Menu/NewGame.jsx
@@ -3,15 +3,20 @@ import RadioGameLength from './RadioGameLength';
 import PropTypes from 'prop-types';
 import { v4 } from 'uuid';
 
-function NewGame(props) {
+class NewGame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      _name: 'null',
+      _players: 'null',
+      _gamelength: 'null'
+    };
+    this.handleNewGameSubmission = this.handleNewGameSubmission.bind(this);
+  }
 
-  let _name = null;
-  let _players = null;
-  let _gamelength = null;
-
-  function handleNewGameSubmission(event) {
+  handleNewGameSubmission(props) {
     event.preventDefault();
-    props.onNewGameSubmission({name: _name.value, players: _players.value, id: v4()
+    props.onNewGameSubmission({name: _name.value, players: _players.value, _gamelength: this.state.selectedGameLength, id: v4()
     });
 
     _name.value = '';
@@ -19,36 +24,37 @@ function NewGame(props) {
     _gamelength.value = '';
   }
 
-  return (
-    <div>
-      <form onSubmit={handleNewGameSubmission}>
-        <p>Your Name:</p>
-        <input
-          type='text'
-          id='name'
-          placeholder='Name'
-          ref={(input) => {_name = input;}}/>
+  render() {
+    return (
+      <div>
+        <form onSubmit={handleNewGameSubmission}>
+          <p>Your Name:</p>
+          <input
+            type='text'
+            id='name'
+            placeholder='Name'
+            ref={(input) => {_name = input;}}/>
 
-        <p>Number of players (between 3 and 5):</p>
-        <input
-          type='number'
-          name='quantity'
-          min='3'
-          max='8'
-          value='3'/>
+          <p>Number of players (between 3 and 5):</p>
+          <input
+            type='number'
+            name='quantity'
+            min='3'
+            max='8'
+            value='3'/>
 
-        <p>Game length:</p>
-        <RadioGameLength />
+          <p>Game length:</p>
+          <RadioGameLength />
 
-        <p><input type="submit" value="Submit"></input></p>
-        <p><input type="reset"></input></p>
-      </form>
-    </div>
-  );
+          <p><input type="submit" value="Submit"></input></p>
+          <p><input type="reset"></input></p>
+        </form>
+      </div>
+    );
+  }
 }
-
 NewGame.propTypes = {
   onNewGameSubmission: PropTypes.func
-}
+};
 
 export default NewGame;
