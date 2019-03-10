bc9b01ca89f4a22412b9678102cfda9207756b1d
Daniel Burbach <dburbach1982@gmail.com>
2019-03-10 16:55:24 -0700

    correct Link and routing namechange

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,7 +12,7 @@ function Main() {
             <h2>A game that makes you a bad person</h2>
           </div>
           <br/>
-          <Link to='/newgame'>New Game</Link>
+          <Link to='/newgameform'>New Game</Link>
           <Link to='/info'>Info</Link>
           <Link to='/fineprint'>Fine Print</Link>
         </div>
