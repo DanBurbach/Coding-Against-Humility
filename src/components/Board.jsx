eb128ced87d617e606d82ca47924308867ec96ac
Daniel Burbach <dburbach1982@gmail.com>
2019-03-10 16:53:11 -0700

    Add routing for display of GameInfo. Points, Player Name, Other players names, and etc

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from './Header';
+import GameInfo from './GameInfo';
 import Deck from './Deck';
 import MainPlayer from './MainPlayer/MainPlayer';
+import JudgePlayer from './JudgePlayer';
 
 function Board(){
   return (
     <div>
       <Header />
+      <GameInfo />
       <Deck />
       <MainPlayer />
+      <JudgePlayer />
     </div>
   );
 }
