ce9d3d377e8f5c9a6bce1c32e453df33c9fbffe7
Daniel Burbach <dburbach1982@gmail.com>
2019-03-10 16:53:44 -0700

    correct class title to className for proper jsx use

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -7,8 +7,8 @@ function Error404() {
   return (
     <div>
       <div id="notfound">
-        <div class="notfound">
-          <div class="notfound-404">
+        <div className="notfound">
+          <div className="notfound-404">
             <h1>404</h1>
             <h2>Page not found</h2>
           </div>
