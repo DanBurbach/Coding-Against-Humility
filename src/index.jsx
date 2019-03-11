4c700aea6b5c686d7a9d47515eeb5cd30f9f576c
Student <example@example.com>
2019-03-11 11:32:24 -0700

    expand use of redux with further setup

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import codingAgainstHumilityReducer from './reducers/coding-against-humility-reducer';
+import { Provider } from 'react-redux';
 
+const store = createStore(codingAgainstHumilityReducer);
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
-      <HashRouter>
+    <HashRouter>
+      <Provider store={store}>
         <Component/>
-      </HashRouter>
-    </AppContainer>,
+      </Provider>
+    </HashRouter>,
     document.getElementById('react-app-root')
   );
 };
