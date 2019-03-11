a94e901f8474ac088ba7257a875a08eca7586043
Student <example@example.com>
2019-03-11 11:32:03 -0700

    expand use of redux with further setup

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,11 +1,9 @@
-import selectedTicketReducer from './selected-ticket-reducer';
-import ticketListReducer from './ticket-list-reducer';
+import codingAgainstHumilityReducer from './coding-against-humility-reducer';
 
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  selectedTicket: selectedTicketReducer,
-  masterTicketList: ticketListReducer
+  codingAgainstHumility: codingAgainstHumilityReducer
 });
 
 export default rootReducer;
