696917d20ebe4cb9a4bb3e398a1572c14f6d7101
Student <example@example.com>
2019-03-11 16:53:38 -0700

    add some minor testing

--- __tests__/startNewGameReducer.test.js
+++ __tests__/startNewGameReducer.test.js
@@ -1,11 +1,12 @@
-describe('codingAgainstHumilityReducer', () => {
+describe('startNewGameReducer', () => {
 
   test('Temporary dummy test: three plus three is six', () => {
     expect(3 + 3).toBe(6);
   });
 
   test('Should return default state if no action type is recognized', () => {
-    expect(codingAgainstHumilityReducer({}, { type: null })).toEqual({});
+    expect(startNewGameReducer({}, { type: null })).toEqual({});
   });
-  
+
+
 });
