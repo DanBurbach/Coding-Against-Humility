39d254e9b896aba3415857617c9adfcf08523a66
Student <example@example.com>
2019-03-13 16:51:28 -0700

    modify header bar links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import NameValue from './Menu/NameValue';
+import GameInfo from './GameInfo';
 import '../assets/styles/Header.css';
-import constants from './../constants';
-const { c } = constants;
+
 
 const Header = () => {
   return (
     <div>
-      <nav id="nav" role="navigation">
+      <nav id="nav">
         <ul>
           <li><Link to='/'>Menu</Link></li>
-          <li><Link to='/Info'>Gameplay</Link></li>
+          <li><Link to='/headergameplay'>Gameplay</Link></li>
           <li><NameValue/></li>
+          <li><GameInfo/></li>
+          <li></li>
         </ul>
       </nav>
     </div>
