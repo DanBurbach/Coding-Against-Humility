189755bf77a85eb0343f3391ae2f94699e65e5f4
Student <example@example.com>
2019-03-13 12:16:02 -0700

    work on connecting to Firebase for storing data and passing information

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,60 +1,87 @@
 import * as types from './../constants/ActionTypes';
 import v4 from 'uuid/v4';
+const { firebaseConfig } = constants;
+import firebase from 'firebase';
 
+firebase.initializeApp(firebaseConfig);
+const gameInfo = firebase.database().ref('gameInfo');
 
-export const addNewGame = (name, gamelength, players ) => ({
-  type: types.ADD_NEWGAME,
-  name,
-  gamelength,
-  players
-});
-
-export const addWhiteCard = (newWhiteCard) => ({
-  type: types.ADD_WHITE_CARD,
-  newWhiteCard
-});
-
-export const removeWhiteCard = (currentWhiteCardID) => ({
-  type: types.REMOVE_WHITE_CARD,
-  currentWhiteCardID
-});
-
-export const addBlackCard = (newBlackCard) => ({
-  type: types.ADD_BLACK_CARD,
-  newBlackCard
-});
-
-export const removeBlackCard = (currentBlackCardID) => ({
-  type: types.REMOVE_BLACK_CARD,
-  currentBlackCardID
-});
-
-export function drawNewBlackCard(blackcards) {
-  return function (dispatch) {
-    const localBlackCardID = v4();
-    dispatch(addBlackCard(blackcards, localBlackCardID));
-    return fetch(
-      // API CALL HERE
-    ).then(
-      response => response.json(),
-      error => console.log('An error did occur when pulling a black card', error)
-    ).then(function(json) {
-      dispatch(addBlackCard);
-    });
-  };
+export function newGame(_name, _gamelength, _players) {
+  return () => gameInfo.push({
+    name: _name,
+    gamelength: _gamelength,
+    players: _players
+  });
 }
-
-export function drawNewWhiteCard(whitecards) {
-  return function (dispatch) {
-    const localWhiteCardID = v4();
-    dispatch(addWhiteCard(whitecards, localWhiteCardID));
-    return fetch(
-      // API CALL HERE
-    ).then(
-      response => response.json(),
-      error => console.log('An error did occur when pulling a white card', error)
-    ).then(function(json) {
-      dispatch(addWhiteCard);
-    });
-  };
-}
+//
+// export const addNewGame = (name, gamelength, players ) => ({
+//   type: types.ADD_NEWGAME,
+//   name,
+//   gamelength,
+//   players
+// });
+//
+// export const userName = (name) => ({
+//   type: types.USER_NAME,
+//   name
+// });
+//
+// export const gameLength = (gamelength) => ({
+//   type: types.GAME_LENGTH,
+//   gamelength
+// });
+//
+// export const numberOfPlayers = (players) => ({
+//   type: types.NUMBER_PLAYERS,
+//   players
+// });
+//
+// export const addWhiteCard = (newWhiteCard) => ({
+//   type: types.ADD_WHITE_CARD,
+//   newWhiteCard
+// });
+//
+// export const removeWhiteCard = (currentWhiteCardID) => ({
+//   type: types.REMOVE_WHITE_CARD,
+//   currentWhiteCardID
+// });
+//
+// export const addBlackCard = (newBlackCard) => ({
+//   type: types.ADD_BLACK_CARD,
+//   newBlackCard
+// });
+//
+// export const removeBlackCard = (currentBlackCardID) => ({
+//   type: types.REMOVE_BLACK_CARD,
+//   currentBlackCardID
+// });
+//
+// export function drawNewBlackCard(blackcards) {
+//   return function (dispatch) {
+//     const localBlackCardID = v4();
+//     dispatch(addBlackCard(blackcards, localBlackCardID));
+//     return fetch(
+//       // API CALL HERE
+//     ).then(
+//       response => response.json(),
+//       error => console.log('An error did occur when pulling a black card', error)
+//     ).then(function(json) {
+//       dispatch(addBlackCard);
+//     });
+//   };
+// }
+//
+// export function drawNewWhiteCard(whitecards) {
+//   return function (dispatch) {
+//     const localWhiteCardID = v4();
+//     dispatch(addWhiteCard(whitecards, localWhiteCardID));
+//     return fetch(
+//       // API CALL HERE
+//     ).then(
+//       response => response.json(),
+//       error => console.log('An error did occur when pulling a white card', error)
+//     ).then(function(json) {
+//       dispatch(addWhiteCard);
+//     });
+//   };
+// }
--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,6 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 import middlewareLogger from './middleware/middleware-logger';
-
 import thunkMiddleware from 'redux-thunk';
 
 const store = createStore(rootReducer, applyMiddleware(middlewareLogger, thunkMiddleware));
