a8e04257e339ea8a7f3217d95626d8fa48398c77
Student <example@example.com>
2019-03-13 13:30:40 -0700

    work on adding firebase to actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
-import * as types from './../constants/ActionTypes';
-import v4 from 'uuid/v4';
+import constants from './../constants';
+// import v4 from 'uuid/v4';
 const { firebaseConfig } = constants;
-import firebase from 'firebase';
+import * as firebase from 'firebase';
 
 firebase.initializeApp(firebaseConfig);
 const gameInfo = firebase.database().ref('gameInfo');
@@ -13,75 +13,71 @@ export function newGame(_name, _gamelength, _players) {
     players: _players
   });
 }
-//
-// export const addNewGame = (name, gamelength, players ) => ({
-//   type: types.ADD_NEWGAME,
-//   name,
-//   gamelength,
-//   players
-// });
-//
-// export const userName = (name) => ({
-//   type: types.USER_NAME,
-//   name
-// });
-//
-// export const gameLength = (gamelength) => ({
-//   type: types.GAME_LENGTH,
-//   gamelength
-// });
-//
-// export const numberOfPlayers = (players) => ({
-//   type: types.NUMBER_PLAYERS,
-//   players
-// });
-//
-// export const addWhiteCard = (newWhiteCard) => ({
-//   type: types.ADD_WHITE_CARD,
-//   newWhiteCard
-// });
-//
-// export const removeWhiteCard = (currentWhiteCardID) => ({
-//   type: types.REMOVE_WHITE_CARD,
-//   currentWhiteCardID
-// });
-//
-// export const addBlackCard = (newBlackCard) => ({
-//   type: types.ADD_BLACK_CARD,
-//   newBlackCard
-// });
-//
-// export const removeBlackCard = (currentBlackCardID) => ({
-//   type: types.REMOVE_BLACK_CARD,
-//   currentBlackCardID
-// });
-//
-// export function drawNewBlackCard(blackcards) {
-//   return function (dispatch) {
-//     const localBlackCardID = v4();
-//     dispatch(addBlackCard(blackcards, localBlackCardID));
-//     return fetch(
-//       // API CALL HERE
-//     ).then(
-//       response => response.json(),
-//       error => console.log('An error did occur when pulling a black card', error)
-//     ).then(function(json) {
-//       dispatch(addBlackCard);
-//     });
-//   };
-// }
-//
-// export function drawNewWhiteCard(whitecards) {
-//   return function (dispatch) {
-//     const localWhiteCardID = v4();
-//     dispatch(addWhiteCard(whitecards, localWhiteCardID));
-//     return fetch(
-//       // API CALL HERE
-//     ).then(
-//       response => response.json(),
-//       error => console.log('An error did occur when pulling a white card', error)
-//     ).then(function(json) {
-//       dispatch(addWhiteCard);
-//     });
-//   };
-// }
+
+export const addNewGame = (name, gamelength, players ) => ({
+  type: types.ADD_NEWGAME,
+  name,
+  gamelength,
+  players
+});
+
+export const userName = (name) => ({
+  type: types.USER_NAME,
+  name
+});
+
+export const gameLength = (gamelength) => ({
+  type: types.GAME_LENGTH,
+  gamelength
+});
+
+export const numberOfPlayers = (players) => ({
+  type: types.NUMBER_PLAYERS,
+  players
+});
+
+export const addWhiteCard = (newWhiteCard) => ({
+  type: types.ADD_WHITE_CARD,
+  newWhiteCard
+});
+
+export const removeWhiteCard = (currentWhiteCardID) => ({
+  type: types.REMOVE_WHITE_CARD,
+  currentWhiteCardID
+});
+
+export const addBlackCard = (newBlackCard) => ({
+  type: types.ADD_BLACK_CARD,
+  newBlackCard
+});
+
+export const removeBlackCard = (currentBlackCardID) => ({
+  type: types.REMOVE_BLACK_CARD,
+  currentBlackCardID
+});
+
+export function drawNewBlackCard(blackCards) {
+  return function (dispatch) {
+    const localBlackCardID = v4();
+    dispatch(addBlackCard(blackCards, localBlackCardID));
+    return fetch('https://codingagainsthumilityapi.firebaseio.com/blackCards').then(
+      response => response.json(),
+      error => console.log('An error did occur when pulling a black card', error)
+    ).then(function(json) {
+      dispatch(addBlackCard);
+    });
+  };
+}
+
+export function drawNewWhiteCard(whiteCards) {
+  return function (dispatch) {
+    const localWhiteCardID = v4();
+    dispatch(addWhiteCard(whiteCards, localWhiteCardID));
+    return fetch('https://codingagainsthumilityapi.firebaseio.com/whiteCards').then(
+      response => response.json(),
+      error => console.log('An error did occur when pulling a white card', error)
+    ).then(function(json) {
+      dispatch(addWhiteCard);
+    });
+  };
+}
