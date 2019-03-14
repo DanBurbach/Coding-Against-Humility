40118db86b6a73f6d550cdeb6f95334d0cb4a5f3
Student <example@example.com>
2019-03-14 08:37:48 -0700

    card details

--- src/components/WhiteCards/WhiteCardDisplay.jsx
+++ src/components/WhiteCards/WhiteCardDisplay.jsx
@@ -7,52 +7,52 @@ function WhiteCardDisplay(props){
     <div className="cards">
       <div className="card">
         <div className="card-face">
-          <div className="card-label">{props.textbyID}</div>
+          <div className="card-label">Racism</div>
         </div>
       </div>
       <div className="card">
         <div className="card-face">
-          <div className="card-label"></div>
+          <div className="card-label">Third base</div>
         </div>
       </div>
       <div className="card">
         <div className="card-face">
-          <div className="card-label"></div>
+          <div className="card-label">A good sniff</div>
         </div>
       </div>
       <div className="card">
         <div className="card-face">
-          <div className="card-label"></div>
+          <div className="card-label">Catapults</div>
         </div>
       </div>
       <div className="card">
         <div className="card-face">
-          <div className="card-label"></div>
+          <div className="card-label">Horse meat</div>
         </div>
       </div>
       <div className="card">
         <div className="card-face">
-          <div className="card-label"></div>
+          <div className="card-label">Making a pouty face</div>
         </div>
       </div>
       <div className="card">
         <div className="card-face">
-          <div className="card-label"></div>
+          <div className="card-label">Man meat</div>
         </div>
       </div>
       <div className="card">
         <div className="card-face">
-          <div className="card-label"></div>
+          <div className="card-label">Kale</div>
         </div>
       </div>
       <div className="card">
         <div className="card-face">
-          <div className="card-label"></div>
+          <div className="card-label">The dentist</div>
         </div>
       </div>
       <div className="card">
         <div className="card-face">
-          <div className="card-label"></div>
+          <div className="card-label">Texas</div>
         </div>
       </div>
     </div>
