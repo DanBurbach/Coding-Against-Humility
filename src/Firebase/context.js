a6298defc12a48d73b53e80036a8bd46aa8a0940
Daniel Burbach <danburbach@Daniels-MacBook-Pro.local>
2019-05-07 23:13:05 -0700

    create new Firebase root file

---
+++ src/Firebase/context.js
@@ -0,0 +1,14 @@
+const FirebaseContext = React.createContext(null);
+
+export const withFirebase = Component => props => ( <
+        FirebaseContext.Consumer > {
+            firebase => < Component {
+                ...props
+            }
+            firebase = {
+                firebase
+            }
+            />} </FirebaseContext.Consumer >
+        );
+
+        export default FirebaseContext;
---
+++ src/Firebase/firebaseConfigDEMO_FORM.js
@@ -0,0 +1,15 @@
+// All below information needs to be updated to use firebase
+// import firebase from 'firebase/app';
+// import 'firebase/database';
+
+// const firebaseConfig = {
+//   apiKey: 'xxxxxx',
+//   authDomain: 'xxxxxx.firebaseapp.com',
+//   databaseURL: 'https://xxxxxx.firebaseio.com',
+//   projectId: 'xxxxxx',
+//   storageBucket: '',
+//   messagingSenderId: 'xxxxxx',
+// };
+// firebase.initializeApp(firebaseConfig);
+
+// export default firebase;
