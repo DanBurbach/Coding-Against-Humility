5c78b4db47f1793a5e14c4b4b87c74dcaff497b5
Daniel Burbach <danburbach@Daniels-MacBook-Pro.local>
2019-05-14 22:09:10 -0700

    update dataflow of login, logout, passwordforget, and signup for user auth

--- src/components/LogIn/index.js
+++ src/components/LogIn/index.js
@@ -3,24 +3,34 @@ import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 
 import { SignUpLink } from '../SignUp';
-import { withFirebase } from '../Firebase';
+import { PasswordForgetLink } from '../PasswordForget';
+import { withFirebase } from '../../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const SignInPage = () => (
   <div>
     <h1>SignIn</h1>
     <SignInForm />
+    <PasswordForgetLink />
     <SignUpLink />
   </div>
 );
 
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null,
+};
+
+const ERROR_CODE_ACCOUNT_EXISTS = 'auth/account-exists-with-different-credential';
+
+const ERROR_MSG_ACCOUNT_EXISTS = `An account with an E-Mail address to this social account already exists. Try to login from this account instead and associate your social accounts on your personal account page.`;
+
 class SignInFormBase extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      email: '',
-      password: '',
-      error: null,
+    this.state = { 
+      ...INITIAL_STATE 
     };
     this.handleSignInFormSubmit = this.handleSignInFormSubmit.bind(this);
     this.handleSignInUpdate = this.handleSignInUpdate.bind(this);
@@ -29,12 +39,15 @@ class SignInFormBase extends Component {
   handleSignInFormSubmit(event) {
     const { email, password } = this.state;
     this.props.firebase
-      .doSignInWithEmailAndPassword(email, password)
+      .signInWithEmailAndPassword(email, password)
       .then(() => {
-        this.setState({ ...INITIAL_STATE });
+        this.setState({...INITIAL_STATE});
         this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
+          if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+            error.message = ERROR_MSG_ACCOUNT_EXISTS;
+          }
         this.setState({ error });
       });
     event.preventDefault();
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
-import { Link,withRouter } from 'react-router-dom';
-import { compose } from 'recompose';
+import { Link, withRouter } from 'react-router-dom';
 
-import { withFirebase } from '../Firebase';
+import { withFirebase } from '../../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const SignUpPage = () => ( 
     <div>
-        <h1> SignUp </h1> 
+        <h1>SignUp</h1> 
         <SignUpForm/>
     </div>
 );
 
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+};
+
+const ERROR_CODE_ACCOUNT_EXISTS = 'auth/email-already-in-use';
+
+const ERROR_MSG_ACCOUNT_EXISTS = `An account with this E-Mail address already exists. Try to login with this account instead. If you think the account is already used from one of the social logins, try to sign in with one of them. Afterward, associate your accounts on your personal account page.`;
+
 class SignUpFormBase extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            username: '',
-            email: '',
-            passwordOne: '',
-            passwordTwo: '',
-            error: null,
-        };
+        this.state = {...INITIAL_STATE};
+
     this.handleSignUpFormSubmit = this.handleSignUpFormSubmit.bind(this);
     this.handleSignUpUpdate = this.handleSignUpUpdate.bind(this);
 
@@ -31,13 +37,25 @@ class SignUpFormBase extends Component {
         const { username, email, passwordOne } = this.state;
 
         this.props.firebase
-            .doCreateUserWithEmailAndPassword(email, passwordOne)
-            .then(authUser => {
-                this.setState({ ...INITIAL_STATE });
+            .createUserWithEmailAndPassword(email, passwordOne)
+            this.props.firebase
+                .user(authUser.user.uid)
+                .set({
+                    username,
+                    email,
+                })
+            .then(() => {
+                this.setState({...INITIAL_STATE});
                 this.props.history.push(ROUTES.HOME);
             })
             .catch(error => {
-                this.setState({ error });
+            if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+                error.message = ERROR_MSG_ACCOUNT_EXISTS;
+            }
+
+            this.setState({
+                error
+            });
             });
 
         event.preventDefault();
@@ -57,15 +75,23 @@ class SignUpFormBase extends Component {
             username === '';
 
         return ( 
-            <form onSubmit = {this.handleSignUpFormSubmit} >
-                <input name = "username" value = {username}
-                    onChange = {this.handleSignUpUpdate} type = "text" placeholder = "Full Name" />
-                <input name = "email" value = {email}
-                    onChange = {this.handleSignUpUpdate} type = "text" placeholder = "Email Address" />
-                <input name = "passwordOne" value = {passwordOne}
-                    onChange = {this.handleSignUpUpdate} type = "password" placeholder = "Password" />
-                <input name = "passwordTwo" value = {passwordTwo}
-                    onChange = {this.handleSignUpUpdate} type = "password" placeholder = "Confirm Password" />
+            <form onSubmit = {this.handleSignUpFormSubmit}>
+                <input name = "username" 
+                    value = {username}
+                    onChange = {this.handleSignUpUpdate} type = "text" 
+                    placeholder = "Full Name"/>
+                <input name = "email" 
+                    value = {email}
+                    onChange = {this.handleSignUpUpdate} type = "text" 
+                    placeholder = "Email Address"/>
+                <input name = "passwordOne" 
+                    value = {passwordOne}
+                    onChange = {this.handleSignUpUpdate} type = "password" 
+                    placeholder = "Password"/>
+                <input name = "passwordTwo" 
+                    value = {passwordTwo}
+                    onChange = {this.handleSignUpUpdate} type = "password" 
+                    placeholder = "Confirm Password" />
                 <button disabled = {isInvalid} type = "submit" > Sign Up </button>
 
                 {error && <p> {error.message}</p>} 
@@ -80,7 +106,7 @@ class SignUpFormBase extends Component {
         </p>
     );
 
-    const SignUpForm = compose( withRouter, withFirebase, )(SignUpFormBase);
+ const SignUpForm = withRouter(withFirebase(SignUpFormBase));
 
     export default SignUpPage;
 
