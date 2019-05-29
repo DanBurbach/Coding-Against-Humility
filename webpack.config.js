c64dce9560f058ad8ece98f928968ec5d6ba5c2c
Daniel Burbach <danburbach@Daniels-MacBook-Pro.local>
2019-05-28 22:52:13 -0700

    minor modifications to core components for security, removal of unneeded node npms, and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,66 @@
 const webpack = require('webpack');
-const { resolve } = require('path');
+const {
+    resolve
+} = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
+    entry: [
+        'babel-polyfill',
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        resolve(__dirname, 'src') + '/index.jsx'
+    ],
+    node: {
+        fs: "empty"},
 
-  entry: [
-    'babel-polyfill',
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    resolve(__dirname, 'src') + '/index.jsx'
-  ],
+    output: {
+        filename: 'app.bundle.js',
+        path: resolve(__dirname, 'build'),
+    },
 
-  output: {
-    filename: 'app.bundle.js',
-    path: resolve(__dirname, 'build'),
-  },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
 
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
+    devtool: '#source-map',
+    devServer: {hot: true,
+        contentBase: resolve(__dirname, 'build')},
 
-  devtool: '#source-map',
-  devServer: {
-    hot: true,
-    contentBase: resolve(__dirname, 'build')
-  },
-
-  module: {
-    rules: [{
-        test: /\.jsx?$/,
-        enforce: 'pre',
-        loader: 'eslint-loader',
-        exclude: /node_modules/,
-        options: {
-          emitWarning: true,
-          configFile: './.eslintrc.json'
-        }
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: [
-              'react-hot-loader/babel',
-              '@babel/plugin-proposal-class-properties'
-            ]
-          }
-        }
-      }, 
-      {
-        test: /\.(png|gif|jp(e*)g|svg)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 8000,
-            name: 'images/[hash]-[name].[ext]'
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
+    module: {
+        rules: [{test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['react-hot-loader/babel',
+                            '@babel/plugin-proposal-class-properties']}
+                }
+            },
+            {test: /\.(png|gif|jp(e*)g|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000,
+                        name: 'images/[hash]-[name].[ext]'}}},
+            {test: /\.css$/,
+                use: ['style-loader',
+                    'css-loader']},
         ]
-      },
-    ]
-  },
+    },
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'template.ejs',
-      appMountId: 'react-app-root',
-      title: 'Coding-Against-Humility',
-      filename: resolve(__dirname, 'build', 'index.html'),
-    }),
-  ]
+    plugins: [
+        new Dotenv(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'template.ejs',
+            appMountId: 'react-app-root',
+            title: 'Coding-Against-Humility',
+            filename: resolve(__dirname, 'build', 'index.html'),
+        }),
+    ]
 };
