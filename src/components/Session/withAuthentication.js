86f167bb4ac1a7df4e3b95afef4cdbc4cc095261
Daniel Burbach <danburbach@Daniels-MacBook-Pro.local>
2019-06-10 22:12:00 -0700

    update withAuthentication HOC for viable use

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
-import AuthUserContext from './AuthContext';
 import firebase from '../../Firebase';
 import { withFirebase } from '../../Firebase/context.js';
+import AuthUserContext from './AuthContext';
 
 const withAuthentication = Component => {
     class WithAuthentication extends Component {
@@ -15,45 +15,47 @@ const withAuthentication = Component => {
             };
         }
 
-    //     componentDidMount() {
-    //         firebase.auth().onAuthStateChanged(authUser => {authUser ? this.setState(authUser) : this.setState(null);
-    //     });
-    //   }
+        //     componentDidMount() {
+        //         firebase.auth().onAuthStateChanged(authUser => {authUser ? this.setState(authUser) : this.setState(null);
+        //     });
+        //   }
 
-        componentDidMount() {
-            this.listener = firebase.auth().onAuthStateChanged(
-                authUser => {
-                    localStorage.setItem('authUser', JSON.stringify(authUser));
-                    this.props.onSetAuthUser(authUser);
-                },
-                () => {
-                    localStorage.removeItem('authUser');
-                    this.props.onSetAuthUser(null);
-                },
-            );
-        }
+    componentDidMount() {
+        this.listener = firebase.auth().onAuthStateChanged(
+            authUser => {
+                localStorage.setItem('authUser', JSON.stringify(authUser));
+                this.props.onSetAuthUser(authUser);
+            },
+            () => {
+                localStorage.removeItem('authUser');
+                this.props.onSetAuthUser(null);
+            },
+        );
+    }
 
-        componentWillUnmount() {
-            this.listener();
-        }
+    componentWillUnmount() {
+        this.listener();
+    }
 
-        render() {
-            return ( 
-                <AuthUserContext.Provider value = {this.state.authUser}>
-                    <Component {...this.props}/>
-                </AuthUserContext.Provider>
+    render() {
+        return ( 
+            <AuthUserContext.Provider value = {this.state.authUser}>
+                <Component {...this.props}/> 
+            </AuthUserContext.Provider>
             );
         }
     }
 
     const mapDispatchToProps = dispatch => ({
         onSetAuthUser: authUser =>
-            dispatch({ type: 'AUTH_USER_SET', authUser }),
+            dispatch({
+                type: 'AUTH_USER_SET',
+                authUser
+            }),
     });
 
     return compose(withFirebase,
-        connect(null, mapDispatchToProps, ),
-    )(WithAuthentication);
+        connect(null, mapDispatchToProps))(WithAuthentication);
 };
 
 export default withAuthentication;
