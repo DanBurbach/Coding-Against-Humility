3d6c6651076bd5555fe3b5cc73a86c5046098670
Daniel Burbach <danburbach@Daniels-MacBook-Pro.local>
2019-06-10 22:09:09 -0700

    include .env data into config information using process.env and Dotenv

--- src/Firebase/context.js
+++ src/Firebase/context.js
@@ -3,9 +3,10 @@ import React from 'react';
 const FirebaseContext = React.createContext(null);
 
 export const withFirebase = Component => props => (
-        <FirebaseContext.Consumer> {firebase => 
-            <Component { ...props }firebase = { firebase } />}  
-        </FirebaseContext.Consumer>
-    );
+    <FirebaseContext.Consumer> 
+    {firebase => 
+        <Component {...props} firebase = { firebase } />}  
+    </FirebaseContext.Consumer>
+);
 
 export default FirebaseContext;
--- src/Firebase/firebaseConfigDEMO_FORM.js
+++ src/Firebase/firebaseConfigDEMO_FORM.js
@@ -2,10 +2,14 @@
 // All below information needs to be updated to use firebase
 // Create an .env file in the main root of the project to use
 
-// process.env.REACT_APP_API_KEY = XXXXxxxx
-// process.env.REACT_APP_AUTH_DOMAIN = xxxxXXXX.firebaseapp.com
-// process.env.REACT_APP_DATABASE_URL = https: //xxxXXXX.firebaseio.com
-// process.env.REACT_APP_PROJECT_ID = xxxxXXXX
-// process.env.REACT_APP_STORAGE_BUCKET = xxxxXXXX.appspot.com
-// process.env.REACT_APP_MESSAGING_SENDER_ID = xxxxXXXX
-// process.env.REACT_APP_APP_ID = xxxxXXXX
+// REACT_APP_API_KEY = XXXXxxxx
+// REACT_APP_AUTH_DOMAIN = xxxxXXXX.firebaseapp.com
+// REACT_APP_DATABASE_URL = https: //xxxXXXX.firebaseio.com
+// REACT_APP_PROJECT_ID = xxxxXXXX
+// REACT_APP_STORAGE_BUCKET = xxxxXXXX.appspot.com
+// REACT_APP_MESSAGING_SENDER_ID = xxxxXXXX
+// REACT_APP_APP_ID = xxxxXXXX
+
+// REACT_APP_CONFIRMATION_EMAIL_REDIRECT = "http://localhost:3000"
+
+// REACT_APP_SECRET_MESSAGE = "hey there!!!"
