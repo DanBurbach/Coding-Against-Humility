bae7ccf159b68518b65a22f89e7cca07679dfd05
DanBurbach <dburbach1982@gmail.com>
2019-06-25 21:33:32 -0700

    Provide Firebase authUser connection to present users displayName and render it on page for gameplay

--- src/components/GameInfo/index.jsx
+++ src/components/GameInfo/index.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Component } from "react";
+import { compose } from "redux";
 import { connect } from "react-redux";
 import { firebaseConnect } from "react-redux-firebase";
 
 
-function GameInfo() {
-  return (
-    <div>
-      <ul>
-        <li>Who's turn it is.</li>
-        <li>Your score.</li>
-        <li>Number of players.</li>
-        {this.props.auth.displayName}
-      </ul>
-    </div>
-  );
+class GameInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayName: this.props.auth.displayName
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Who's turn it is.</li>
+          <li>Your score.</li>
+          <li>Number of players.</li>
+          {this.props.auth.displayName}
+        </ul>
+      </div>
+    );
+  }
 }
 
-
-const enhance = connect(
-  ({ firebase: { auth, profile } }) => ({ auth, profile,})
-)
+const enhance = compose(
+  connect(({ firebase: { auth, profile } }) => ({ auth, profile }))
+);
 
 export default firebaseConnect()(enhance(GameInfo));
