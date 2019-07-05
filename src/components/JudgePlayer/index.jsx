1f335cdd6552f8c91209b947f1c0f25d8e016e27
DanBurbach <dburbach1982@gmail.com>
2019-07-04 22:42:18 -0700

    begin framework of JudgePlayer deal black card logic, will have to merge with Black Card Component

--- src/components/JudgePlayer/index.jsx
+++ src/components/JudgePlayer/index.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function JudgePlayer() {
-  return (
-    <div>
-      {/* <form onSubmit={event => {
-        event.preventDefault();
-        dispatch();
-      }}></form> */}
-      <button>Deal</button>
-    </div>
-  );
+class JudgePlayer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blackCard = 0
+    };
+    this.handleDealBlack = this.handleDealBlack.bind(this);
+  }
+
+  handleDealBlack = () => {
+
+      // const rand = min + Math.random() * (max - min);
+      // this.setState({ random: this.state.blackCard + rand });
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <div className="blackCardDelt">{this.state.blackCard}</div> */}
+        <button onClick={this.handleDealBlack}>Deal</button>
+      </div>
+    );
+  }
 }
-
 export default JudgePlayer;
