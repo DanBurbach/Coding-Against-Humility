cb210627b0d9b73ec91fa8a4ba63a6646baf2936
DanBurbach <dburbach1982@gmail.com>
2019-07-05 21:41:42 -0700

    deal button on judgeplayer created to slice from blackCard json file array at random

--- src/components/JudgePlayer/index.jsx
+++ src/components/JudgePlayer/index.jsx
@@ -4,18 +4,30 @@ class JudgePlayer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      blackCard = 0
+      blackCard: 0
     };
     this.handleDealBlack = this.handleDealBlack.bind(this);
   }
 
-  handleDealBlack = () => {
-      const min = 0;
-      const max = 414;
-      document.write(blackCards[0].text);
-      document.write(blackCards[0].pick);
-      const rand = min + Math.random() * (max - min);
-      this.setState({ random: this.state.blackCard + rand });
+//there are 0-414 (415 total) black cards in the json deck
+
+  handleDealBlack = (event) => {
+      event.preventDefault();
+      const blackCardData = require('../../../decks/BlackCards.json');
+      const blackDeckSize = 414;
+      let x = Math.floor(Math.random() * blackDeckSize);
+      let blackCardSlice = blackCardData.slice(x, x+1);
+      console.log(blackCardSlice);
+      // this.setState({ random: this.state.blackCard })
+
+
+
+      // const min = 0;
+      // const max = 414;
+      // document.write(blackCards[0].text);
+      // document.write(blackCards[0].pick);
+      // const rand = min + Math.random() * (max - min);
+      // this.setState({ random: this.state.blackCard + rand });
   }
 
   render() {
