a594e878c9590549ee084f8355eac6eb0066449a
DanBurbach <dburbach1982@gmail.com>
2019-07-14 15:50:43 -0700

    update and remove extra component to allow for addition of points, reset of total points, and a temporary total of all points

--- src/components/Scoreboard/index.js
+++ src/components/Scoreboard/index.js
@@ -3,19 +3,38 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firebaseConnect } from 'react-redux-firebase';
 
-class ScoreCounter extends Component {
-    constructor() {
-        super(props);
-        this.state = {
-            scoreCounters: [{
-                id: 1,
-                value: 0
-            }]
-        };
-        this.handleScoreIncrease = this.handleScoreIncrease.bind(this);
-        this.handleReset = this.handleReset.bind(this);
+class Counter extends Component {
+    render() {
+        const { onIncrement } = this.props;
+        return ( 
+            <div>
+                <span> {this.formatCount()} </span> 
+                <button onClick = {() => onIncrement(this.props.counter)}>Add </button>
+            </div>
+        );
     }
-    handleScoreIncrease(counter) {
+
+    formatCount() {
+        const { value } = this.props.counter;
+        return value;
+    }
+}
+
+class ScoreBoard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            counters: [
+                { id: 1, value: 0 },
+                { id: 2, value: 0 },
+                { id: 3, value: 0 },
+                { id: 4, value: 0 },
+                { id: 5, value: 0 }
+            ],
+            total: 0
+        };
+    }
+    handleIncrement(counter) {
         const total = this.state.total + 1;
         const counters = [...this.state.counters];
         const index = counters.indexOf(counter);
@@ -27,39 +46,53 @@ class ScoreCounter extends Component {
         });
     }
 
+    handleDecrement(counter) {
+        const total = this.state.total - 1;
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {
+            ...counter
+        };
+        counters[index].value--;
+        this.setState({
+            counters: counters,
+            total: total
+        });
+    }
+
     handleReset() {
         const total = 0;
-        const counters = this.state.counters.map(y => {
-            y.value = 0;
-            return y;
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
         });
         this.setState({
             counters: counters,
             total: total
-        })
+        });
     }
 
     render() {
         return ( 
-            <div>
-                <button onClick = {this.handleReset}> 
-                    Reset Score 
-                </button> 
-                {this.state.counters.map(counter => ( 
-                <CountRecorder 
-                    key = {counter.id}
-                    onIncrement = {this.handleScoreIncrease}
-                    counter = {counter}
-                />
+            <div >
+            <button onClick = {this.handleReset.bind(this)}> Reset </button> {
+                this.state.counters.map(counter => ( 
+                    <Counter 
+                        key = {counter.id}
+                        onIncrement = {this.handleIncrement.bind(this)}
+                        onDecrement = {this.handleDecrement.bind(this)}
+                        counter = { counter }
+                    />
                 ))
-                } 
-            <div> Total: {this.state.total} </div> 
+            } <div> Total: {
+                this.state.total
+            } </div> 
             </div>
-        )
+        );
     }
 }
 const enhance = compose(
     connect(({firebase: {auth, profile}}) => ({auth, profile}))
 );
 
-export default firebaseConnect()(enhance(ScoreCounter));
+export default firebaseConnect()(enhance(ScoreBoard));
