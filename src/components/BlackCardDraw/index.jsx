7df50dcc37940e38cff2d1caba9c1adbff294821
DanBurbach <dburbach1982@gmail.com>
2019-07-16 10:48:11 -0700

    work on setting up an animation using ReactCSSTransitionGroup to flip the black card on button click, animation format will be used potentially later on white card hand deal

--- src/components/BlackCardDraw/index.jsx
+++ src/components/BlackCardDraw/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { compose } from "redux";
 import { connect } from 'react-redux';
 import { firebaseConnect } from 'react-redux-firebase';
-// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import '../../assets/styles/dealBlackCardButton.css';
 import '../../assets/styles/BlackDeckDisplay.css';
 
@@ -16,14 +16,15 @@ class BlackCardDraw extends Component {
     this.handleDrawNewBlackCard = this.handleDrawNewBlackCard.bind(this);
     this.handleDealBlack = this.handleDealBlack.bind(this);
     this.handleFlipBlackCard = this.handleFlipBlackCard.bind(this);
+    this.renderBlackCard = this.renderBlackCard.bind(this);
   }
 
 //there are 0-414 (415 total) black cards in the json deck
 
   handleDrawNewBlackCard = (event) => {
-    event.preventDefault();
-    this.handleDealBlack();
-    this.handleFlipBlackCard();
+      event.preventDefault();
+      this.handleDealBlack();
+      this.handleFlipBlackCard();
   }
 
   handleDealBlack = () => {
@@ -38,44 +39,62 @@ class BlackCardDraw extends Component {
   }
 
   handleFlipBlackCard = () => {
-      let cards = document.getElementsByClassName('blackcard-content-container');
-
-      for (var i = 0; i < cards.length; i++) {
-        var card = cards[i];
-        if (card.classList.contains('blackcard-back')) {
-          card.classList.remove('blackcard-back');
-        } else {
-          card.classList.add('blackcard-back');
-        }
-      }
+      this.setState({
+        flipBlackCard: !this.state.flipBlackCard
+      })
     }
 
-  render() {
-  return (
-    <div className='item-wrapper'>
-      <div className='row'>
-        <div className='col-4'></div>
-        <div className='col-4'>
-          <div className='blackcard-card'>
-            <div className='blackcard-container'>
-              <div className='blackcard-front blackcard-side'>
-                <div className='blackcard-content'>
-                  <h1>Coding Against Humility</h1>
-                </div>
-              </div>
-              <div className='blackcard-back blackcard-side'>
-                <div className='blackcard-content-back'>
-                  {this.state.blackCard}
-                </div>
-              </div>
+  renderBlackCard = () => {
+    return (
+      <div className="blackcard-card">
+        <div className="blackcard-container">
+          <div className="blackcard-front blackcard-side">
+            <div className="blackcard-content">
+              <h1>Coding Against Humility</h1>
+            </div>
+          </div>
+          <div className="blackcard-back blackcard-side">
+            <div className="blackcard-content-back">
+              {this.state.blackCard}
             </div>
           </div>
         </div>
-        <div className='col-4'></div>
       </div>
-    <button onClick={this.handleDrawNewBlackCard} id='dealBlackCard'>Deal Black Card</button>
-    </div>
-    )
+    );
+  }
+
+
+
+  render() {
+    return (
+      <div className="item-wrapper">
+        <div className="row">
+          <div className="col-4" />
+
+          <div className="col-4">
+            <div className="container">
+              <div className="animation-container">
+                <ReactCSSTransitionGroup
+                  transitionEnterTimeout={500}
+                  transitionLeaveTimeout={500}
+                  transitionAppearTimeout={500}
+                  // transitionIn={flipBlackCard}
+                  transitionAppear={true}
+                  transitionName="flip"
+                >
+                  {this.renderBlackCard()}
+                </ReactCSSTransitionGroup>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-4" />
+        </div>
+        <button onClick={this.handleDrawNewBlackCard} id="dealBlackCard">
+          Deal Black Card
+        </button>
+      </div>
+    );
   }
 }
 
