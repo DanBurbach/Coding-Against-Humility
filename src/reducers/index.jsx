7b36e0b4c82d469ea8923f609f9b21fe8c114748
DanBurbach <dburbach1982@gmail.com>
2019-07-31 13:55:15 -0700

    update reducer library used and eliminated extra reducers not being used

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,9 +1,4 @@
 import startNewGameViewReducer from "./startNewGameReducer";
-// import scorePointsReducer from './scorePointsReducer';
-// import dealBlackCardsReducer from './dealBlackCardsReducer';
-// import dealWhiteCardsReducer from './dealWhiteCardsReducer';
-// import gameLengthReducer from './gameLengthReducer';
-// import numberOfPlayersReducer from './numberOfPlayersReducer';
 import sessionReducer from "./sessions";
 import userReducer from "./user";
 import authReducer from "./authReducer";
@@ -12,12 +7,7 @@ import { firebaseReducer } from "react-redux-firebase";
 import { combineReducers } from "redux";
 
 const gameReducers = combineReducers({
-  // dealBlackCards: dealBlackCardsReducer,
-  // dealWhiteCards: dealWhiteCardsReducer,
-  // scorePoints: scorePointsReducer,
   startNewGameView: startNewGameViewReducer,
-  // gameLength: gameLengthReducer,
-  // numberOfPlayers: numberOfPlayersReducer,
   sessionState: sessionReducer,
   userState: userReducer,
   auth: authReducer,
